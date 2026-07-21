bb6a1cc1fff966c10e79df75c54a790cb9ff111c
Zeitgeist Jones <teachervin77777@gmail.com>
2026-07-21 19:42:34 -0400

    tripwire offline fix

--- app/providers.js
+++ app/providers.js
@@ -1,7 +1,6 @@
 "use client";
-import { WagmiProvider, createConfig, http } from "wagmi";
+import { WagmiProvider, createConfig, http, injected } from "wagmi";
 import { base } from "wagmi/chains";
-import { injected } from "wagmi/connectors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const config = createConfig({
